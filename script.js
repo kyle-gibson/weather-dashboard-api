19403bfd86c190fea83172d325f1a945be846358
Kyle Gibson <kyle.gibson22@yahoo.com>
2021-01-24 20:16:37 -0800

    working on UV api call

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+let lat = "";
+let lon = "";
+
 $(document).ready(function() {
     $("#search-btn").on("click", function(){
         const userInput = $("#search-input").val();          
             searchCurrent(userInput);    
     })   
 
-    apiKey="&appid=28a251366afe42620dacd55b2d9f72b3";
+    var apiKey="&appid=28a251366afe42620dacd55b2d9f72b3";
 
     function searchCurrent(userInput){ 
-        const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + apiKey + "&units=imperial";
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + apiKey + "&units=imperial";
+        
     $.ajax({
       url: queryURL,
-      method: "GET"
-      //promise
+      method: "GET"     
     }).then(function(response) {
         $("#current-weather").empty()
         console.log(response)
@@ -19,59 +22,44 @@ $(document).ready(function() {
         let cityName = $("<h5>").addClass("card-title").text(response.name);
         let windSpeed = $("<p>").addClass("card-text").text("Windspeed: " + response.wind.speed + " MPH");
         let humidity = $("</p>").addClass("card-text").text("Humidity: " + response.main.humidity + "%");
-        let temperature = $("</p>").addClass("card-text").text("Current Temperature: " + response.main.temp * 1.80 + 32 + " °F");  
+        let temperature = $("</p>").addClass("card-text").text("Current Temperature: " + response.main.temp + " °F");
+        //let getUV = $("<p>").addClass("card-text").text("UV: " + response.wind.speed);
+          
             
-            console.log(response.main.temp * 1.80 + 32)
-
         let cardBody = $("<div>").addClass("card-body")
         let iconImage = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
             //https://openweathermap.org/img/w/01n.png
 
         let cardContainer = $("<div>").addClass("card")
 
-        cityName.append(iconImage)
-        cardBody.append(cityName, temperature, humidity, windSpeed)
-        cardContainer.append(cardBody)
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        console.log(lat, lon);
+
+        cityName.append(iconImage);
+        cardBody.append(cityName, temperature, humidity, windSpeed);
+        cardContainer.append(cardBody);
         
-        $("#current-weather").append(cardContainer)
+        $("#current-weather").append(cardContainer);
+
+        getUV (); //calling function to get UV information
 
     });
+};
+ 
+    // function getUV(){
+    //     var apiKey = "&appid=28a251366afe42620dacd55b2d9f72b3";
+    //     let queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon" + lon + apiKey + "&units=imperial";
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"     
+    //       }).then(function(response) {             
+    //           console.log(response)    
+    //     });
 
-}
-
-
-
-
-
-
-
-   
-
-
-
-
-
+};
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Wrap around everything, document.ready function    
 });
